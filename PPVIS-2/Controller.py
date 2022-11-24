a947bebe3208d309b96cfbfb5d0ad66fe498879a
repLLEH <73431596+repLLEH@users.noreply.github.com>
2022-11-24 17:29:00 +0300

    Add files via upload

---
+++ PPVIS-2/Controller.py
@@ -0,0 +1,26 @@
+import Model
+class DiaryController:
+    def __init__(self):
+        pass
+    def start(self):
+        pass
+    def userAuth(self):
+        pass
+    def userReg(self):
+        pass
+    def userProfile(self,id):
+        pass
+    def userProfileDeletion(self,id):
+        pass
+    def userProfileModification(self):
+        pass
+    def noteChoice(self,profile_id):
+        pass
+    def taskCreation(self,profile_id,date):
+        pass
+    def annotationCreation(self,profile_id,date):
+        pass
+    def showNoteList(self,profile_id,date):
+        pass
+    def noteModification(self,note_id,date):
+        pass
