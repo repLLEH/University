ca15617716f4cd3a6aa13c7cc698d94da89f73be
repLLEH <73431596+repLLEH@users.noreply.github.com>
2023-01-08 00:31:19 +0300

    Add files via upload

--- mrzvis/lab2/main.py
+++ mrzvis/lab2/main.py
@@ -1,31 +1,26 @@
-from neural_network import *
+from HopfieldNet import *
 
 if __name__ == '__main__':
-
+    hopnet=HopfieldNetwork()
     while(True):
         print('training = 1')
         print('recognize = 2')
-
         choice = (input())
-
-
         if int(choice) == 1:
-            file_name = 'image.txt'
-            models = readFromFile(file_name)
-            matrix = getWeightFromModels(models)
+            file_name = 'images'
+            models = hopnet.readFromFile(file_name)
+            print(models)
+            matrix = hopnet.getWeightFromModels(models)
             for i in matrix:
                 print(i)
             matrix_file_name = 'matrix.txt'
-            writeInFile(matrix_file_name, matrix)
-
-
-
+            hopnet.writeInFile(matrix_file_name, matrix)
         elif int(choice) == 2:
-            model_file_name = 'image.txt'
+            model_file_name = 'image1.txt'
             matrix_file_name = 'matrix.txt'
-            matrix = getMatrix(matrix_file_name)
-            model = readFromFile(model_file_name)
+            matrix = hopnet.getMatrix(matrix_file_name)
+            model = hopnet.readFromFile(model_file_name)
 
-            run(model, matrix)
+            hopnet.run(model, matrix)
 
 
--- mrzvis/lab3/main.py
+++ mrzvis/lab3/main.py
@@ -1,104 +1,72 @@
-from Predict import Predict
-def file_open_data_test(file_name,data_test,input_train,output_train):
-    with open(file_name, 'r') as td:
-        while True:
-            out_train = []
-            lines = td.readline()
-            if not lines:
-                break
-            data_test += 1
-            ipd, gt = lines.split("->")
-            in_train = list(map(int, ipd.split(" ")))
-            out_train.append(gt)
-            out_train = list(map(int, out_train))
-            input_train.append(in_train)
-            output_train.append(out_train)
-    td.close()
-    return data_test
-def file_open_input(file_name,data_test,input_train,output_train):
-    with open(file_name, 'r') as td:
-        while True:
-            out_train = []
-            lines = td.readline()
-            if not lines:
-                break
-            data_test += 1
-            ipd, gt = lines.split("->")
-            in_train = list(map(int, ipd.split(" ")))
-            out_train.append(gt)
-            out_train = list(map(int, out_train))
-            input_train.append(in_train)
-            output_train.append(out_train)
-    td.close()
-    return input_train
-def file_open_output(file_name,data_test,input_train,output_train):
-    with open(file_name, 'r') as td:
-        while True:
-            out_train = []
-            lines = td.readline()
-            if not lines:
-                break
-            data_test += 1
-            ipd, gt = lines.split("->")
-            in_train = list(map(int, ipd.split(" ")))
-            out_train.append(gt)
-            out_train = list(map(int, out_train))
-            input_train.append(in_train)
-            output_train.append(out_train)
-    td.close()
-    return output_train
-
-def main():
-    sequence = Predict(lr=0.02, init_range=0.3)
-    data = 0
-    data_test = 0
-    training_accuracy = 0
-    testing_accuracy = 0
-    input_train = []
-    output_train = []
-    input_test = []
-    output_test = []
-
-
-    data = file_open_data_test("train_data.txt",data_test,input_train,output_train)
-    input_train = file_open_input("train_data.txt",data_test,input_train,output_train)
-    output_train= file_open_output("train_data.txt",data_test,input_train,output_train)
-    data_test=file_open_data_test("test_data.txt", data_test, input_test, output_test)
-    input_test=file_open_input("test_data.txt", data_test, input_test, output_test)
-    output_test=file_open_output("test_data.txt", data_test, input_test, output_test)
-    print(input_train)
-    print(output_train)
-    for i in range(3000):
-        cost = 0
-        for j in range(len(input_train)):
-            cost += sequence.train(input_train[j], output_train[j])
-            
-        if i % 100 == 0:
-            correct = 0
-            print('Epoch:', i)
-            for k in range(len(input_train)):
-                if sequence.predict(input_train[k]) == output_train[k]:
-                    correct += 1
-                training_accuracy = correct / data
-            print('loss:', cost / data)
-            print('train accuracy:', training_accuracy)
-
-    correct_test = 0
-    print('test result:')
-    for l in range(len(input_test)):
-        if sequence.predict(input_test[l]) == output_test[l]:
-            correct_test +=1
-        testing_accuracy = correct_test / data_test
-    print('test accuracy', testing_accuracy)
-
-
-
-    print([1, 2, 3], '->', sequence.predict([1, 2, 3]))
-    print([3, 2, 1], '->', sequence.predict([3, 2, 1]))
-    print([3, 2, 2], '->', sequence.predict([3, 2, 2]))
-    print([0, 1, 0], '->', sequence.predict([0, 1, 0]))
-    print([1, 2, 3, 4], '->', sequence.predict([1, 2, 3, 4]))
-
-
-if __name__ == "__main__":
-    main()
+import numpy as np
+import keras
+import tensorflow as tf
+def fibonacci(n):
+    a, b = 1, 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+seq = [('X^3', list(map(lambda x: x*x*x, [i for i in range(10)]))),
+        ('x+1', [i for i in range(10)]),
+        ('fibonacci', [fibonacci(10)])]
+def sequence(seq_data):
+    list = [0, 0, 0, 0]
+    list[0] = seq_data[0]
+    list[1] = seq_data[1]
+    list[2] = seq_data[2]
+    list[3] = seq_data[3]
+    inp_layer = np.array(list)
+    inp_layer = np.array(inp_layer[2])
+    return inp_layer
+def make_smth(seq_data):
+    list = [0, 0, 0, 0]
+    list[0] = seq_data[0]
+    list[1] = seq_data[1]
+    list[2] = seq_data[2]
+    list[3] = seq_data[3]
+    inp_layer = np.array(list)
+    list = inp_layer.reshape((inp_layer.shape[0], 2, 2, 1))
+    return list
+def lstm():
+    lstm_model.add(keras.layers.TimeDistributed(keras.layers.convolutional.Conv1D(filters=64,  activation='tanh'),))
+    lstm_model.add(keras.layers.TimeDistributed(keras.layers.Flatten()))
+    lstm_model.add(keras.layers.LSTM(50, activation='relu'))
+    lstm_model.add(keras.layers.Dense(1))
+    lstm_model.compile(loss='mse')
+lstm_model = keras.models.Sequential()
+lstm()
+while(True):
+    print("X^3===>1")
+    print("X+1===>2")
+    print("Fibonaci===>3")
+    print("Exit===>0")
+    choice=input()
+    if (choice==1):
+        inp_layer, list = sequence(seq[0]),make_smth(seq[0])
+        inp_layer.reshape((1, 2, 2, 1))
+        out_layer = np.array([seq[i] for i in range(3, len(seq[1]))])
+        lstm_model.fit(list, out_layer, epochs=500, verbose=0)
+        final = lstm_model.predict(inp_layer, verbose=0)
+        #round(int(final), 2)
+        print("Sequence: ",seq[0])
+        print("Result ", inp_layer, ": ", round(int(final), 2))
+    elif (choice == 2):
+        inp_layer, list = sequence(seq[1]), make_smth(seq[1])
+        inp_layer.reshape((1, 2, 2, 1))
+        out_layer = np.array([seq[i] for i in range(3, len(seq[1]))])
+        lstm_model.fit(list, out_layer, epochs=500, verbose=0)
+        final = lstm_model.predict(inp_layer, verbose=0)
+        # round(int(final), 2)
+        print("Sequence: ", seq[1])
+        print("Result ", inp_layer, ": ", round(int(final), 2))
+    elif (choice == 3):
+        inp_layer, list = sequence(seq[2]), make_smth(seq[2])
+        inp_layer.reshape((1, 2, 2, 1))
+        out_layer = np.array([seq[i] for i in range(3, len(seq[2]))])
+        lstm_model.fit(list, out_layer, epochs=500, verbose=0)
+        final = lstm_model.predict(inp_layer, verbose=0)
+        # round(int(final), 2)
+        print("Sequence: ", seq[2])
+        print("Result ", inp_layer, ": ", round(int(final), 2))
+    elif (choice == 0):
+        exit(0)
